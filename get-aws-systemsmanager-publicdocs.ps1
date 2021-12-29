98c57887eb0b872fe296f13d0ba0f36662674910
Trevor Sullivan <trevor@trevorsullivan.net>
2021-12-28 18:21:05 -0700

    Create get-aws-systemsmanager-publicdocs.ps1

---
+++ get-aws-systemsmanager-publicdocs.ps1
@@ -0,0 +1,14 @@
+Import-Module -Name AWS.Tools.SimpleSystemsManagement
+
+$Filter = [Amazon.SimpleSystemsManagement.Model.DocumentKeyValuesFilter]::new()
+$Filter.Key = 'Owner'
+$Filter.Values = 'Public'
+
+$ScriptDate = Get-Date -Format yyyy-MM-dd
+$DocList = Get-SSMDocumentList -Filter $Filter
+
+foreach ($Document in $DocList) {
+  $CurrentDocument = Get-SSMDocument -Name $Document.Name -DocumentVersion $Document.DocumentVersion
+  $FileName = '{0}/{1}-{2}-{3}.json' -f $ScriptDate, $Document.Owner, $CurrentDocument.Name, $Document.DocumentVersion
+  $CurrentDocument | ConvertTo-Json -Depth 10 | Set-Content -Path $FileName
+}

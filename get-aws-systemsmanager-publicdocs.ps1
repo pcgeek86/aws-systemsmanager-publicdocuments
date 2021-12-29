a187948cc93703ee19c4b4224b2450d4a8a9db3a
Trevor Sullivan <trevor@trevorsullivan.net>
2021-12-28 19:13:24 -0700

    Update get-aws-systemsmanager-publicdocs.ps1

--- get-aws-systemsmanager-publicdocs.ps1
+++ get-aws-systemsmanager-publicdocs.ps1
@@ -1,11 +1,13 @@
-Import-Module -Name AWS.Tools.SimpleSystemsManagement
+Import-Module -Name AWS.Tools.SimpleSystemsManagement, SimplySql
+
 Set-DefaultAWSRegion -Region us-west-2
 
 $Filter = [Amazon.SimpleSystemsManagement.Model.DocumentKeyValuesFilter]::new()
 $Filter.Key = 'Owner'
 $Filter.Values = 'Public'
 
-$ScriptDate = Get-Date -Format yyyy-MM-dd
+$ScriptDate = Get-Date
+$TargetFolder = 'documents/{0}' -f (Get-Date -Format 'yyyy/MM/dd')
 
 # Create directory for files, if it doesn't exist.
 New-Item -ItemType Directory -Path $ScriptDate -ErrorAction Ignore
@@ -14,6 +16,6 @@ $DocList = Get-SSMDocumentList -Filter $Filter
 
 foreach ($Document in $DocList) {
   $CurrentDocument = Get-SSMDocument -Name $Document.Name -DocumentVersion $Document.DocumentVersion
-  $FileName = '{0}/{1}-{2}-{3}.json' -f $ScriptDate, $Document.Owner, $CurrentDocument.Name.Split('/')[-1], $Document.DocumentVersion
+  $FileName = '{0}/{1}-{2}-{3}.json' -f $TargetFolder, $Document.Owner, $CurrentDocument.Name.Split('/')[-1], $Document.DocumentVersion
   $CurrentDocument | ConvertTo-Json -Depth 10 | Set-Content -Path $FileName
 }
